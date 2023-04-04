e818b4c0ce1cbe45b738cc9704b5c8d5f06d124e
danilo <danilo@aritech.com>
2023-04-04 14:33:07 +0200

    Añade swagger a la API
    127.0.0.1:8000/swagger-ui/

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api_schema', get_schema_view(title='API Schema', description='Guia para la API REST del proyecto Almacen RFID'), name='api_schema'),
+    path('swagger-ui/', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url':'api_schema'}), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('api.urls'))
 ]
